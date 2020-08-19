9c10dd57722a6bc8400c2edd40b112fd34c690c4
Alec Brooks <alec.e.brooks@gmail.com>
2020-08-19 21:27:37 +1000

    Batch render sprints for FPS improvement

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,16 +1,20 @@
 use ggez;
 use ggez::graphics;
+use ggez::graphics::spritebatch::SpriteBatch;
 use ggez::graphics::Color;
 use ggez::graphics::DrawParam;
 use ggez::graphics::Image;
 use ggez::nalgebra as na;
+use ggez::timer;
 use ggez::Context;
+use itertools::Itertools;
 use specs::{join::Join, Read, ReadStorage, System};
+use std::collections::HashMap;
+use std::time::Duration;
 
 use crate::components::*;
-use crate::constants::*;
+use crate::constants::TILE_WIDTH;
 use crate::resources::{Gameplay, Time};
-use std::time::Duration;
 
 pub struct RenderingSystem<'a> {
     pub context: &'a mut Context,
@@ -30,23 +34,49 @@ impl<'a> System<'a> for RenderingSystem<'a> {
         // clearing the screen
         graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
 
-        // Get renderables with their positions and sort by z
-        // This will visually layer entities
-        let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
-        rendering_data.sort_by(|&a, &b| a.0.z.partial_cmp(&b.0.z).expect("expected comparison"));
+        // Get all renderables with positions
+        let rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
+        let mut rendering_batches: HashMap<u8, HashMap<String, Vec<DrawParam>>> = HashMap::new();
 
-        // Iterate through all positions and renderables, load the image and draw it
+        // Iterate through all positions and renderables, determine which path should be rendered
+        // and at which drawparams and then add it to rendering batches
         for (position, renderable) in rendering_data.iter() {
-            let image = self.get_image(renderable, time.delta);
+            let image_path = self.get_image(renderable, time.delta);
             let x = position.x as f32 * TILE_WIDTH;
             let y = position.y as f32 * TILE_WIDTH;
+            let z = position.z;
 
-            let draw_params = DrawParam::new().dest(na::Point2::new(x, y));
-            graphics::draw(self.context, &image, draw_params).expect("exected render");
+            let draw_param = DrawParam::new().dest(na::Point2::new(x, y));
+            rendering_batches
+                .entry(z)
+                .or_default()
+                .entry(image_path)
+                .or_default()
+                .push(draw_param);
+        }
+
+        for (_z, group) in rendering_batches
+            .iter()
+            .sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
+        {
+            for (image_path, draw_params) in group {
+                let image = Image::new(self.context, image_path).expect("expected image");
+                let mut sprite_batch = SpriteBatch::new(image);
+
+                for draw_param in draw_params.iter() {
+                    sprite_batch.add(*draw_param);
+                }
+
+                graphics::draw(self.context, &sprite_batch, graphics::DrawParam::new())
+                    .expect("expected render");
+            }
         }
 
         self.draw_text(&gameplay.state.to_string(), 525.0, 80.0);
         self.draw_text(&gameplay.moves_count.to_string(), 525.0, 100.0);
+        let fps = format!("FPS: {:.0}", timer::fps(self.context));
+        self.draw_text(&fps, 525.0, 120.0);
+
         // Present the context, actually displays
         graphics::present(self.context).expect("expected to present");
     }
@@ -68,13 +98,12 @@ impl RenderingSystem<'_> {
         )
         .expect("expected drawing queued text");
     }
-    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> Image {
+    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> String {
         let path_index = match renderable.kind() {
             RenderableKind::Static => 0,
             RenderableKind::Animated => ((delta.as_millis() % 1000) / 250) as usize,
         };
-        let image_path = renderable.path(path_index);
-
-        Image::new(self.context, image_path).expect("expected image")
+        // Image::new(self.context, image_path).expect("expected image")
+        renderable.path(path_index)
     }
 }
